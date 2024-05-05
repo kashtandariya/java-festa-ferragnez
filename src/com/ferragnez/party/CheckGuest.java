005cd1b3b30ea737af98f64841d3cfc6640e4c6b
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-05 17:50:03 +0200

    add: commenti + condizione

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -5,14 +5,25 @@ import java.util.Scanner;
 public class CheckGuest {
 	public static void main(String[] args) {
 		
+		//stringa invitati
 		String[] listaInvitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax",
 				"Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei",
 				"Martina Maccherone", "Rachel Zeilic"};
 		
+		//chiedi nome utente
 		try (Scanner scan = new Scanner(System.in)) {
 			System.out.println("Inserisci il tuo nome:");
 			String nomeUtente = scan.nextLine();
-		}
+		
+		//check condizione, se l'utente è nella listaInvitati
+		//cicla tutta la lista
+		boolean presente = false;
+	    for (String invitato : listaInvitati) {
+	        if (invitato.equalsIgnoreCase(nomeUtente)) {
+	            presente = true;
+	            break;
+	        }
+	    }
 
 	}
 }
