4df07dd881510f168a800eacab8962ea6bf0c2dc
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-05 17:46:45 +0200

    add: array invitati

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,12 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+	public static void main(String[] args) {
+		
+		String[] listaInvitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax",
+				"Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei",
+				"Martina Maccherone", "Rachel Zeilic"};
+	}
+}
