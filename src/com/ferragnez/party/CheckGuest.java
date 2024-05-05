f16b9509a7a90c3e37b277b2621ce9613fd9e835
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-05 17:56:47 +0200

    add: condizione finale e esercizio rifatto con commenti più precisi

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -5,25 +5,34 @@ import java.util.Scanner;
 public class CheckGuest {
 	public static void main(String[] args) {
 		
-		//stringa invitati
+		//stringa listaInvitati
 		String[] listaInvitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax",
 				"Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei",
 				"Martina Maccherone", "Rachel Zeilic"};
 		
-		//chiedi nome utente
+		//chiedi all'utente il proprio nome utilizzando lo Scanner input
 		try (Scanner scan = new Scanner(System.in)) {
 			System.out.println("Inserisci il tuo nome:");
 			String nomeUtente = scan.nextLine();
-		
-		//check condizione, se l'utente è nella listaInvitati
-		//cicla tutta la lista
+			
+		/*crea condizione per capire se l'utente è invitato o meno, ciclando con il ciclo
+		for ogni nome*/
 		boolean presente = false;
 	    for (String invitato : listaInvitati) {
-	        if (invitato.equalsIgnoreCase(nomeUtente)) {
-	            presente = true;
-	            break;
-	        }
-	    }
-
+	    if (invitato.equalsIgnoreCase(nomeUtente)) {
+			presente = true;
+			break;
+			}
+		}
+	    
+	    // Stampa il risultato controllando con le condizionali la condizione di presenza
+		if (presente) {
+		System.out.println("Benvenuto alla festa!");
+		} else {
+		System.out.println("Mi dispiace, non sei nella lista degli invitati.");
+		}
+		
 	}
+		
+  }
 }
